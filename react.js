7623fcf0891cefba146f2ce5021a2f87fbd1a242
mozisan <cc.sig@icloud.com>
2021-09-07 22:59:25 +0900

    Update

--- react.js
+++ react.js
@@ -19,6 +19,7 @@ module.exports = {
             reservedFirst: ['key', 'ref'],
           },
         ],
+        'react/prop-types': 'off',
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
       },
