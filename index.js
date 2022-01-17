3dc2e67315982e6e2ffe93a2077b392590090573
mozisan <cc.sig@icloud.com>
2022-01-18 03:20:45 +0900

    Fix some rule options

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ module.exports = {
     ],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'unicorn/consistent-function-scoping': [
+      'error',
+      {
+        checkArrowFunctions: false,
+      },
+    ],
     'unicorn/filename-case': [
       'error',
       {
