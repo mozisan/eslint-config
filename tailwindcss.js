84dbb0405c6f4a2f4ebeaba67c10e903291d3c91
('3') <mozisan@users.noreply.github.com>
2023-02-15 09:29:36 +0900

    Add config for TailwindCSS

---
+++ tailwindcss.js
@@ -0,0 +1,25 @@
+/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+module.exports = {
+	overrides: [
+		{
+			files: ["*.tsx"],
+			plugins: ["tailwindcss"],
+			rules: {
+				"tailwindcss/enforces-negative-arbitrary-values": "error",
+				"tailwindcss/enforces-shorthand": "error",
+				"tailwindcss/migration-from-tailwind-2": "error",
+				"tailwindcss/no-custom-classname": "error",
+				"tailwindcss/no-contradicting-classname": "error",
+			},
+		},
+		{
+			files: ["tailwind.config.?(c)js"],
+			env: {
+				node: true,
+			},
+			rules: {
+				"unicorn/prefer-module": "off",
+			},
+		},
+	],
+};
